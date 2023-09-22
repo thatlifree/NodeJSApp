fc43fc339f9e6eb14a7e8dbc1f2c73cf711e8614
Valerii Halkin <thatlifree@gmail.com>
2023-09-22 10:52:52 +0300

    11

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const HOST = process.env.HOST || 'localhost';
 const app = express();
 
 app.get('/', (req, res) => {
-  return res.send(`Hello World! To be or not to be. That is a question`);
+  return res.send(`Hello World! To be or not to be. That is a question. 1 more thing`);
 });
 
 app.listen(PORT);
